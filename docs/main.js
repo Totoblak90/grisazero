a3bb9540d7fc666e3656cfbf325ccc7f3522cbfe
Tobias Blaksley <tobias.blaksleyiraola@cordialfinanciera.com.ar>
2022-01-27 23:20:34 -0300

    project init

--- docs/main.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    var style = window.getComputedStyle(document.getElementById("header"), null);
-    style.getPropertyValue("height");
-
-    const main = document.getElementById('main')
-    main.style.marginTop = style.getPropertyValue("height");
-
-}
